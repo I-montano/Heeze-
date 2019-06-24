155ae3818c8889ad19454251da74f0ddf902e4d1
JavierPoduje <javier.poduje@gmail.com>
2019-06-24 09:59:51 -0400

    Inicio

---
+++ heeze/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for heeze project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heeze.settings')
+
+application = get_wsgi_application()
